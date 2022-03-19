aa8523399715da2f51facca395c4e36537fc426a
Max Hahn <max.vhahn@gmail.com>
2022-03-19 14:07:04 -0400

    First Commit
    First commit

---
+++ BobTaborFollowAlong/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobTaborFollowAlong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+
+
+
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42};
+
+            //string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
+            /*
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+            */
+            /*
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */
+
+            /*
+            string zig = "You can get what you want out of life " +
+                "if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
+
+
+            string myName = "Maxwell Hahn";
+
+            char[] charNameArray = myName.ToCharArray();
+            Array.Reverse(charNameArray);
+
+            foreach (char charName in charNameArray)
+            {
+                Console.WriteLine(charName);
+            }
+            Console.ReadLine();
+            */
+
+            /*numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+            */
+
+            //Console.WriteLine("Bob's Big Giveaway!");
+            //Console.WriteLine("Choose a door: 1, 2, or 3");
+            //string userInput = Console.ReadLine();
+
+            //string message = (userInput == "1") ? "boat" : "strand of lint";
+            ////Console.WriteLine("You won a {0}.", message);
+            //Console.WriteLine("You entered {0}, therefore you won a {1}.", userInput, message);
+
+            //Console.ReadLine();
+
+            //string message = "";
+
+            //if (userInput == "1")
+            //    message = "You won a new car!";
+            //else if (userInput == "2")
+            //    message = "You won a new boat!";
+            //else if (userInput == "3")
+            //    message = "You won a new cat!";
+            //else
+            //{
+            //    message = "Sorry, we didn't understand.";
+            //    message += "You lose.";
+            //}
+
+            //Console.WriteLine(message);
+            //Console.ReadLine();
+
+            //    Console.WriteLine("The Name Game");
+
+            //    Console.WriteLine("What's your name?");
+            //    string firstName = Console.ReadLine();
+
+            //    Console.WriteLine("What's your last name?");
+            //    string lastName = Console.ReadLine();
+
+            //    Console.Write("In what city were you born?");
+            //    string city = Console.ReadLine();
+
+            //    char[] firstNameArray = firstName.ToCharArray();
+            //    Array.Reverse(firstNameArray);
+
+            //    char[] lastNameArray = lastName.ToCharArray();
+            //    Array.Reverse(lastNameArray);
+
+            //    char[] cityArray = city.ToCharArray();
+            //    Array.Reverse(cityArray);
+
+            //    string result = " ";
+
+            //    foreach (char item in firstNameArray)
+            //    {
+            //        result += item;
+            //    }
+
+            //    result += " ";
+
+            //    foreach (char item in lastNameArray)
+            //    {
+            //        result += " ";
+            //    }
+
+            //    result += " ";
+
+            //    foreach (char item in cityArray)
+            //    {
+            //        result += " ";
+            //    }
+
+            //    Console.WriteLine("Results: " + result);
+
+            //    Console.ReadLine();
+            //}
+
+            //private static void Reverse String()
+            //{
+            //    char[] firstNameArray = firstName.ToCharArray()
+            //    Array.Reverse(firstNameArray);
+            //}
+
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            /*
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            //string myLastName;
+            //Console.WriteLine("Type your last name: ");
+            //myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+            */
+
+
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = " ";
+
+            if (userValue == "1")
+            {
+                message = "You won a new car!";
+            }
+            else if (userValue == "2")
+            {
+                message = "You won a new boat!";                
+            }
+            else if (userValue == "3")
+            {
+                message = "You won a new cat!";                
+            }
+            else
+            {
+                message = "Sorry, we didn't understand that.";
+                message += "You Lose.";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+            //Console.WriteLine("You won a {0}.", message);
+            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
+
+            Console.ReadLine();
+            */
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+                
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+
+            }
+
+
+            Console.ReadLine();
+            */
+            /*
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+            */
+        
+        
+    }
+}
